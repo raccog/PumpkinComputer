1b602cb456ea3eeabd34f51eba673e8f78e689bf
Ryan Cohen <rcohenprogramming@gmail.com>
2023-08-26 16:33:45 -0400

    Add common simulator functions to a main test bench class

--- Makefile
+++ Makefile
@@ -23,16 +23,17 @@ open-vcd-uart_tx: uart_tx.vcd
 	gtkwave $<
 .PHONY: open-vcd-uart_tx
 
-$(BUILD_DIR)/verilated.o:
+$(BUILD_DIR)/verilated.o: /usr/share/verilator/include/verilated.cpp \
+		/usr/share/verilator/include/verilated_threads.cpp \
+		/usr/share/verilator/include/verilated_vcd_c.cpp
 	g++ -shared -fPIC -I /usr/share/verilator/include \
 		-I /usr/share/verilator/include/vltstd \
-		/usr/share/verilator/include/verilated.cpp \
-		/usr/share/verilator/include/verilated_threads.cpp \
-		/usr/share/verilator/include/verilated_vcd_c.cpp \
+		$^ \
 		-O2 \
 		-o "$@"
 
-$(BUILD_DIR)/uart_tx: sim/uart_tx.cpp $(BUILD_DIR)/obj_dir/Vuart_tx__ALL.a $(BUILD_DIR)/verilated.o
+$(BUILD_DIR)/uart_tx: sim/uart_tx.cpp sim/sim_common.h \
+	$(BUILD_DIR)/obj_dir/Vuart_tx__ALL.a $(BUILD_DIR)/verilated.o
 	-mkdir -p "$(@D)"
 	g++ -I /usr/share/verilator/include \
 		-I $(BUILD_DIR)/obj_dir/ \
---
+++ sim/sim_common.h
@@ -0,0 +1,53 @@
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <string>
+
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+#define VPRINTF(...) \
+    if (tb.verbose()) printf(__VA_ARGS__)
+
+template <class VerilatorTestBench>
+class MainTestBench {
+public:
+    MainTestBench(std::string moduleName) : m_tb{new VerilatorTestBench}, m_vcd{new VerilatedVcdC}, m_moduleName{moduleName}, m_verbose{false}, m_tickCount{1} {
+        Verilated::traceEverOn(true);
+        m_tb->trace(m_vcd.get(), 99);
+        m_vcd->open((m_moduleName + ".vcd").c_str());
+    }
+
+    std::shared_ptr<VerilatorTestBench> signals() {return m_tb;}
+    void parseArgs(int argc, char **argv) {
+        // Check for verbose command argument
+        if (argc > 1) {
+            for (int i = 1; i < argc; ++i) {
+                if (strcmp(argv[i], "-v") == 0) {
+                    m_verbose = true;
+                }
+            }
+        }
+    }
+    void tick() {
+        m_tb->eval();
+        m_vcd->dump(m_tickCount * 10 - 2);
+        m_tb->i_clk = 1;
+        m_tb->eval();
+        m_vcd->dump(m_tickCount * 10);
+        m_tb->i_clk = 0;
+        m_tb->eval();
+        m_vcd->dump(m_tickCount * 10 + 5);
+        m_vcd->flush();
+        m_tickCount += 1;
+    }
+    bool verbose() {return m_verbose;}
+private:
+    std::shared_ptr<VerilatorTestBench> m_tb;
+    std::unique_ptr<VerilatedVcdC> m_vcd;
+    std::string m_moduleName;
+    bool m_verbose;
+    uint64_t m_tickCount;
+};
+
