c1338184138c94092b95a516c23dab1d653a6295
Ryan Cohen <rcohenprogramming@gmail.com>
2023-09-02 11:30:22 -0400

    dmi_jtag: Rename types and enums to be more consistent

--- rtl/dmi_jtag.sv
+++ rtl/dmi_jtag.sv
@@ -24,25 +24,25 @@ module dmi_jtag
     output logic o_td
 );
     typedef enum logic [3:0] {
-        Exit2Dr, Exit1Dr, ShiftDr, PauseDr, SelectIrScan, UpdateDr,
-        CaptureDr, SelectDrScan, Exit2Ir, Exit1Ir, ShiftIr, PauseIr,
-        RunTestIdle, UpdateIr, CaptureIr, TestLogicReset
-    } jtag_tap_state;
+        EXIT2_DR, EXIT1_DR, SHIFT_DR, PAUSE_DR, SELECT_IR_SCAN, UPDATE_DR,
+        CAPTURE_DR, SELECT_DR_SCAN, EXIT2_IR, EXIT1_IR, SHIFT_IR, PAUSE_IR,
+        RUN_TEST_IDLE, UPDATE_IR, CAPTURE_IR, TEST_LOGIC_RESET
+    } jtag_tap_state_e;
 
     typedef enum logic [IR_WIDTH-1:0] {
-        Bypass0 = 'h00,
-        IdCode = 'h01,
-        Dtmcs = 'h10,
-        Dmi = 'h11,
-        Bypass1 = 'h1f
-    } jtag_instruction;
+        BYPASS0 = 'h00,
+        IDCODE = 'h01,
+        DTMCS = 'h10,
+        DMI = 'h11,
+        BYPASS1 = 'h1f
+    } jtag_instruction_e;
 
     typedef struct packed {
         logic _reserved;
         logic [10:0] manufid;
         logic [15:0] partnumber;
         logic [3:0] version;
-    } jtag_idcode;
+    } jtag_idcode_t;
     
     typedef struct packed {
         logic [3:0] version;
@@ -53,22 +53,22 @@ module dmi_jtag
         logic dmireset;
         logic dmihardreset;
         logic [13:0] _reserved1;
-    } jtag_dtmcs;
+    } jtag_dtmcs_t;
 
     typedef struct packed {
         logic [1:0] op;
         logic [31:0] data;
         logic [DMI_ADDR_WIDTH-1:0] address;
-    } jtag_dmi;
+    } jtag_dmi_t;
 
     localparam int unsigned DMI_REG_WIDTH = 33+DMI_ADDR_WIDTH;
-    localparam jtag_idcode REG_IDCODE = '{
+    localparam jtag_idcode_t REG_IDCODE = '{
         version:'hf,
         partnumber:'hffff,
         manufid:'h7ff,
         _reserved:'b1
     };
-    localparam jtag_dtmcs DTMCS_RESET = '{
+    localparam jtag_dtmcs_t DTMCS_RESET = '{
         version:'h1,
         abits:6'(DMI_ADDR_WIDTH),
         dmistat:'h0,
@@ -79,9 +79,9 @@ module dmi_jtag
         _reserved1:'h0
     };
 
-    jtag_tap_state next_state, current_state;
-    jtag_instruction current_instruction;
-    jtag_dmi reg_dmi;
+    jtag_tap_state_e next_state, current_state;
+    jtag_instruction_e current_instruction;
+    jtag_dmi_t reg_dmi;
     logic [DMI_REG_WIDTH:0] reg_shift;
     logic o_td_latch;
     logic [1:0] dmistat;
@@ -90,88 +90,88 @@ module dmi_jtag
 
     always_comb begin
         case (current_state)
-            TestLogicReset:
+            TEST_LOGIC_RESET:
                 if (i_tms)
-                    next_state = TestLogicReset;
+                    next_state = TEST_LOGIC_RESET;
                 else
-                    next_state = RunTestIdle;
-            RunTestIdle:
+                    next_state = RUN_TEST_IDLE;
+            RUN_TEST_IDLE:
                 if (i_tms)
-                    next_state = SelectDrScan;
+                    next_state = SELECT_DR_SCAN;
                 else
-                    next_state = RunTestIdle;
-            SelectDrScan:
+                    next_state = RUN_TEST_IDLE;
+            SELECT_DR_SCAN:
                 if (i_tms)
-                    next_state = SelectIrScan;
+                    next_state = SELECT_IR_SCAN;
                 else
-                    next_state = CaptureDr;
-            CaptureDr, ShiftDr:
+                    next_state = CAPTURE_DR;
+            CAPTURE_DR, SHIFT_DR:
                 if (i_tms)
-                    next_state = Exit1Dr;
+                    next_state = EXIT1_DR;
                 else
-                    next_state = ShiftDr;
-            Exit1Dr:
+                    next_state = SHIFT_DR;
+            EXIT1_DR:
                 if (i_tms)
-                    next_state = UpdateDr;
+                    next_state = UPDATE_DR;
                 else
-                    next_state = PauseDr;
-            PauseDr:
+                    next_state = PAUSE_DR;
+            PAUSE_DR:
                 if (i_tms)
-                    next_state = Exit2Dr;
+                    next_state = EXIT2_DR;
                 else
-                    next_state = PauseDr;
-            Exit2Dr:
+                    next_state = PAUSE_DR;
+            EXIT2_DR:
                 if (i_tms)
-                    next_state = UpdateDr;
+                    next_state = UPDATE_DR;
                 else
-                    next_state = ShiftDr;
-            UpdateDr, UpdateIr:
+                    next_state = SHIFT_DR;
+            UPDATE_DR, UPDATE_IR:
                 if (i_tms)
-                    next_state = SelectDrScan;
+                    next_state = SELECT_DR_SCAN;
                 else
-                    next_state = RunTestIdle;
-            SelectIrScan:
+                    next_state = RUN_TEST_IDLE;
+            SELECT_IR_SCAN:
                 if (i_tms)
-                    next_state = TestLogicReset;
+                    next_state = TEST_LOGIC_RESET;
                 else
-                    next_state = CaptureIr;
-            CaptureIr, ShiftIr:
+                    next_state = CAPTURE_IR;
+            CAPTURE_IR, SHIFT_IR:
                 if (i_tms)
-                    next_state = Exit1Ir;
+                    next_state = EXIT1_IR;
                 else
-                    next_state = ShiftIr;
-            Exit1Ir:
+                    next_state = SHIFT_IR;
+            EXIT1_IR:
                 if (i_tms)
-                    next_state = UpdateIr;
+                    next_state = UPDATE_IR;
                 else
-                    next_state = PauseIr;
-            PauseIr:
+                    next_state = PAUSE_IR;
+            PAUSE_IR:
                 if (i_tms)
-                    next_state = Exit2Ir;
+                    next_state = EXIT2_IR;
                 else
-                    next_state = PauseIr;
-            Exit2Ir:
+                    next_state = PAUSE_IR;
+            EXIT2_IR:
                 if (i_tms)
-                    next_state = UpdateIr;
+                    next_state = UPDATE_IR;
                 else
-                    next_state = ShiftIr;
+                    next_state = SHIFT_IR;
             default:
-                next_state = TestLogicReset;
+                next_state = TEST_LOGIC_RESET;
         endcase
     end
 
-    initial current_state = TestLogicReset;
+    initial current_state = TEST_LOGIC_RESET;
     always_ff @ (posedge i_tck) begin
         current_state <= next_state;
     end
 
-    initial current_instruction = IdCode;
+    initial current_instruction = IDCODE;
     always_ff @ (negedge i_tck) begin
         case (current_state)
-            TestLogicReset:
-                current_instruction <= IdCode;
-            UpdateIr:
-                current_instruction <= jtag_instruction'(reg_shift[IR_WIDTH-1:0]);
+            TEST_LOGIC_RESET:
+                current_instruction <= IDCODE;
+            UPDATE_IR:
+                current_instruction <= jtag_instruction_e'(reg_shift[IR_WIDTH-1:0]);
             default: begin end
         endcase
     end
@@ -179,28 +179,28 @@ module dmi_jtag
     initial reg_shift = 0;
     always_ff @ (posedge i_tck) begin
         case (current_state)
-            CaptureIr:
+            CAPTURE_IR:
                 reg_shift[IR_WIDTH-1:0] <= current_instruction;
-            CaptureDr:
+            CAPTURE_DR:
                 case (current_instruction)
-                    Dmi:
+                    DMI:
                         reg_shift <= reg_dmi;
-                    IdCode:
+                    IDCODE:
                         reg_shift[31:0] <= REG_IDCODE;
-                    Dtmcs:
+                    DTMCS:
                         // TODO: Update idle with minimum cycles (currently set to
                         // 1 cycle in Run-Test/Idle)
                         // TODO: Ensure dmistat gets set properly here
-                        reg_shift[31:0] <= DTMCS_RESET | (int'(dmistat) << 10);
+                        reg_shift[31:0] <= DTMCS_RESET | (jtag_dtmcs_t'(dmistat) << 10);
                     default: begin end
                 endcase
-            ShiftIr:
+            SHIFT_IR:
                 reg_shift[IR_WIDTH-1:0] <= {i_td, reg_shift[IR_WIDTH-1:1]};
-            ShiftDr:
+            SHIFT_DR:
                 case (current_instruction)
-                    Dmi:
+                    DMI:
                         reg_shift <= {i_td, reg_shift[DMI_REG_WIDTH:1]};
-                    IdCode, Dtmcs:
+                    IDCODE, DTMCS:
                         reg_shift[31:0] <= {i_td, reg_shift[31:1]};
                     default: begin end
                 endcase
@@ -210,7 +210,7 @@ module dmi_jtag
 
     initial o_td_latch = 0;
     always_ff @ (negedge i_tck) begin
-        if (current_state == ShiftIr || current_state == ShiftDr)
+        if (current_state == SHIFT_IR || current_state == SHIFT_DR)
             o_td_latch <= reg_shift[0];
         else
             o_td_latch <= 0;
@@ -218,12 +218,12 @@ module dmi_jtag
 
     initial reg_dmi = 0;
     always_ff @ (negedge i_tck) begin
-        if (current_state == UpdateDr)
-            if (current_instruction == Dmi)
+        if (current_state == UPDATE_DR)
+            if (current_instruction == DMI)
                 // TODO: Process DMI transaction here
                 // TODO: Update dmistat with error codes
                 reg_dmi <= reg_shift;
-            else if (current_instruction == Dtmcs)
+            else if (current_instruction == DTMCS)
                 // TODO: Cancel DMI transaction when dmihardreset is written
                 if (reg_shift[16] || reg_shift[17]) begin
                     dmistat <= 0;
